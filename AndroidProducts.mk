b3ed7918505e1c826e5472ecc28fcb763bc3d387
Hadenix <vsegda_online_007@bk.ru>
2023-08-12 19:54:09 +0300

    breeze: Initial TWRP device tree
    Made with SebaUbuntu's TWRP device tree generator
    Arch: arm64
    Manufacturer: umidigi
    Model: A5 Pro
    
    * A5_Pro-user 9 PPR1.180610.011 1616484719 release-keys

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_A5_Pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_A5_Pro-user \
+    omni_A5_Pro-userdebug \
+    omni_A5_Pro-eng
---
+++ omni_A5_Pro.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from A5_Pro device
+$(call inherit-product, device/umidigi/A5_Pro/device.mk)
+
+PRODUCT_DEVICE := A5_Pro
+PRODUCT_NAME := omni_A5_Pro
+PRODUCT_BRAND := UMIDIGI
+PRODUCT_MODEL := A5_Pro
+PRODUCT_MANUFACTURER := umidigi
+
+PRODUCT_GMS_CLIENTID_BASE := android-agold
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="A5_Pro-user 9 PPR1.180610.011 1616484719 release-keys"
+
+BUILD_FINGERPRINT := UMIDIGI/A5_Pro/A5_Pro:9/PPR1.180610.011/1616484719:user/release-keys
