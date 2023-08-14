7322fb2fc0a598e069648738f69c99b8613e624e
Hadenix <vsegda_online_007@bk.ru>
2023-08-14 15:46:30 +0300

    breeze: Use device parameter TW_BRIGHTNESS_PATH to set brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,3 +95,4 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
